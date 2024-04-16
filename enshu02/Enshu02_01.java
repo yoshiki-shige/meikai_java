7886fdcd32d29391b1b04541831b78a93e7488f6
yoshiki-shige <yoshiki.shige.ddthink@gmail.com>
2024-04-16 10:19:04 +0900

    test

---
+++ enshu02/Enshu02_01.java
@@ -0,0 +1,30 @@
+package enshu02;
+/*クラス名:enshu02
+ *概要:enshu02_01のクラス宣言
+ *作成者:Y.Shige
+ *作成日:2024/04/15
+ */
+public class Enshu02_01 {
+	/*関数名:main
+	 *概要:
+	 *引数:なし
+	 *戻り値:なし
+	 *作成者:Y.Shige
+	 *作成日:2024/04/15
+	 */
+	public static void main(String[] args) {
+		int x;
+		int y;
+		
+		x = 63.5;
+		y = 18.5;
+		
+		System.out.println("xの値は" + x + "です。");
+		System.out.println("yの値は" + y + "です。");
+		System.out.println("合計は" + (x + y) + "です。");
+		System.out.println("平均は" + (x + y) / 2 + "です。");
+	
+		
+	}
+
+}
