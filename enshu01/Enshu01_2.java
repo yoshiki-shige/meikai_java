0749245404566e20a6112e108301a3cd0dc2d40d
yoshiki-shige <yoshiki.shige.ddthink@gmail.com>
2024-04-16 10:18:23 +0900

    test

---
+++ enshu01/Enshu01_2.java
@@ -0,0 +1,24 @@
+package enshu01;
+/*クラス名:Enshu01_2
+ *概要:Enshu01_2のクラス宣言
+ *作成者:Y.Shige
+ *作成日:2024/04/15
+ */
+
+public class Enshu01_2 {
+	/*関数名:main
+	 *概要:重良輝を縦に1文字ずつ表示させる
+	 *引数:なし
+	 *戻り値:なし
+	 *作成者:Y.Shige
+	 *作成日:2024/04/15
+	 */
+	public static void main(String[] args) {
+		//重良輝を縦に1文字ずつ表示させる
+		System.out.println("重");
+        System.out.println("良");
+        System.out.print("輝");
+
+	}
+
+}
---
+++ enshu01/Enshu01_3.java
@@ -0,0 +1,24 @@
+package enshu01;
+/*クラス名:Enshu01_3
+ *概要:Enshu01_3のクラス宣言
+ *作成者:Y.Shige
+ *作成日:2024/04/15
+ */
+public class Enshu01_3 {
+	/*関数名:main
+	 *概要:重良輝を縦に1文字ずつ、姓と名を空けて表示させる
+	 *引数:なし
+	 *戻り値:なし
+	 *作成者:Y.Shige
+	 *作成日:2024/04/15
+	 */
+	public static void main(String[] args) {
+		//重良輝を縦に1文字ずつ、姓と名を空けて表示させる
+		System.out.println("重");
+		System.out.println();
+		System.out.println("良");
+		System.out.println("輝");
+
+	}
+
+}
